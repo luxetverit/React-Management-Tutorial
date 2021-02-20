1f15f366d73807b15caccb2406dcff21100f7b4b
luxetverit <replaysoccer@naver.com>
2021-02-20 23:30:06 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Customer from "./components/Customer";
+import "./App.css";
+
+const customers = [
+    {
+        id: 1,
+        image: "https://placeimg.com/64/64/1",
+        name: "정우남",
+        birthday: "890815",
+        gender: "남자",
+        job: "대학생",
+    },
+    {
+        id: 2,
+        image: "https://placeimg.com/64/64/2",
+        name: "홍홍홍",
+        birthday: "900111",
+        gender: "남자",
+        job: "대학생",
+    },
+    {
+        id: 3,
+        image: "https://placeimg.com/64/64/3",
+        name: "순신",
+        birthday: "790911",
+        gender: "남자",
+        job: "백수",
+    },
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop managemnet system!</h2>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {customers.map((c) => {
+                    return (
+                        <Customer
+                            key={c.id}
+                            id={c.id}
+                            image={c.image}
+                            name={c.name}
+                            birthday={c.birthday}
+                            gender={c.gender}
+                            job={c.job}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
