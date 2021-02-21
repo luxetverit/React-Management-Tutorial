13ba37000811c92afe650fe5f77a1a4cdfcc35f3
luxetverit <replaysoccer@naver.com>
2021-02-21 20:09:36 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
@@ -18,16 +19,39 @@ const styles = (theme) => ({
     table: {
         minWidth: 1080,
     },
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
 });
+/* React 라이프사이클
 
+1. constructor()
+2. componentwillMount()
+3. render()
+4. componentDidMount()
+
+*/
+/*
+
+컴포넌트에 props 또는 state 변경의 경우
+shouldComponentUpdate()
+
+*/
 class App extends Component {
     state = {
         customers: "",
+        completed: 0,
     };
 
+    //API를 불러와서 WEB STIE 화면에 특정한 view 출력하고자 할 때
+    //아래 함수에서 API 비동기적 호출
     componentDidMount() {
+        this.timer = setInterval(this.progress, 15);
         this.callApi()
             .then((res) => this.setState({ customers: res }))
+            //API에서 응답이 response
+            //setState를 통해 상태 변화를 주고, 그 변화를 react가 감지
+
             .catch((err) => console.log(err));
     }
 
@@ -35,6 +59,13 @@ class App extends Component {
         const response = await fetch("/api/customers");
         const body = await response.json();
         return body;
+    };
+
+    progress = () => {
+        const { completed } = this.state;
+        this.setState({
+            completed: completed >= 100 ? 0 : completed + 1,
+        });
     };
 
     render() {
@@ -53,21 +84,31 @@ class App extends Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.customers
-                            ? this.state.customers.map((c) => {
-                                  return (
-                                      <Customer
-                                          key={c.id}
-                                          id={c.id}
-                                          image={c.image}
-                                          name={c.name}
-                                          birthday={c.birthday}
-                                          gender={c.gender}
-                                          job={c.job}
-                                      />
-                                  );
-                              })
-                            : ""}
+                        {this.state.customers ? (
+                            this.state.customers.map((c) => {
+                                return (
+                                    <Customer
+                                        key={c.id}
+                                        id={c.id}
+                                        image={c.image}
+                                        name={c.name}
+                                        birthday={c.birthday}
+                                        gender={c.gender}
+                                        job={c.job}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan="6" align="center">
+                                    <CircularProgress
+                                        className={classes.progress}
+                                        variant="determinate"
+                                        value={this.state.completed}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        )}
                         ;
                     </TableBody>
                 </Table>
